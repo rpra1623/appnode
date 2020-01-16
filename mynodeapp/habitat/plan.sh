190a31ca7c3134b27321830e3ddd3fec6efe197a
rpra1623 <58035658+rpra1623@users.noreply.github.com>
2020-01-16 12:33:35 +0530

    Update plan.sh
    1

--- mynodeapp/habitat/plan.sh
+++ mynodeapp/habitat/plan.sh
@@ -1,5 +1,5 @@
 pkg_origin=myorigin
-pkg_name=mytutorialapp
+pkg_name=mytutorialapp1
 pkg_version="0.2.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_upstream_url=https://github.com/habitat-sh/habitat-example-plans
